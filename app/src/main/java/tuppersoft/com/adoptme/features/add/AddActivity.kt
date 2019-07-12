94ef8aa255fe87bb824af5fdfcf09d416fcbd2ca
Raúl Rodríguez Concepción <>
2019-07-12 14:59:56 +0200

    Adding images

--- app/src/main/java/tuppersoft/com/adoptme/features/add/AddActivity.kt
+++ app/src/main/java/tuppersoft/com/adoptme/features/add/AddActivity.kt
@@ -2,7 +2,6 @@ package tuppersoft.com.adoptme.features.add
 
 import android.content.Intent
 import android.graphics.Bitmap
-import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.Menu
@@ -14,11 +13,20 @@ import tuppersoft.com.adoptme.core.platform.GlobalActivity
 
 class AddActivity : GlobalActivity() {
 
+    companion object {
+        private const val REQUEST_IMAGE_CAPTURE_1 = 1
+        private const val REQUEST_IMAGE_CAPTURE_2 = 2
+        private const val REQUEST_IMAGE_CAPTURE_3 = 3
+        private const val REQUEST_IMAGE_CAPTURE_4 = 4
+
+        private const val DATA = "data"
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         appComponent.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(tuppersoft.com.adoptme.R.layout.activity_add)
-
 
         setSupportActionBar(toolbar)
         val mActionBar = supportActionBar
@@ -26,11 +34,15 @@ class AddActivity : GlobalActivity() {
         mActionBar?.setDisplayShowHomeEnabled(true)
         mActionBar?.title = getString(tuppersoft.com.adoptme.R.string.add)
 
+        card1.setOnClickListener { dispatchTakePictureIntent(REQUEST_IMAGE_CAPTURE_1) }
+        card2.setOnClickListener { dispatchTakePictureIntent(REQUEST_IMAGE_CAPTURE_2) }
+        card3.setOnClickListener { dispatchTakePictureIntent(REQUEST_IMAGE_CAPTURE_3) }
+        card4.setOnClickListener { dispatchTakePictureIntent(REQUEST_IMAGE_CAPTURE_4) }
+
+
         customRadio.setOnClick {
             Toast.makeText(this@AddActivity, it.toString(), Toast.LENGTH_LONG).show()
         }
-
-        card1.setOnClickListener { dispatchTakePictureIntent() }
 
         customRadioSex.setOnClick { }
     }
@@ -40,22 +52,29 @@ class AddActivity : GlobalActivity() {
         return true
     }
 
-    private lateinit var file: Uri
-    private val REQUEST_IMAGE_CAPTURE = 1
 
-    private fun dispatchTakePictureIntent() {
+    private fun dispatchTakePictureIntent(who: Int) {
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
             takePictureIntent.resolveActivity(packageManager)?.also {
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                startActivityForResult(takePictureIntent, who)
             }
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            val imageBitmap = data?.extras?.get("data") as Bitmap
+        if (requestCode == REQUEST_IMAGE_CAPTURE_1 && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get(DATA) as Bitmap
             image1.setImageBitmap(imageBitmap)
+        } else if (requestCode == REQUEST_IMAGE_CAPTURE_2 && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get(DATA) as Bitmap
+            image2.setImageBitmap(imageBitmap)
+        } else if (requestCode == REQUEST_IMAGE_CAPTURE_3 && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get(DATA) as Bitmap
+            image3.setImageBitmap(imageBitmap)
+        } else if (requestCode == REQUEST_IMAGE_CAPTURE_4 && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get(DATA) as Bitmap
+            image4.setImageBitmap(imageBitmap)
         }
     }
 
