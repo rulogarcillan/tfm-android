46df7fc714da06e7615605aeee48ef296cc2530d
Raúl Rodríguez Concepción <>
2019-07-12 13:28:39 +0200

    images

--- app/src/main/java/tuppersoft/com/adoptme/features/add/AddActivity.kt
+++ app/src/main/java/tuppersoft/com/adoptme/features/add/AddActivity.kt
@@ -1,6 +1,10 @@
 package tuppersoft.com.adoptme.features.add
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.Menu
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_add.*
@@ -26,6 +30,8 @@ class AddActivity : GlobalActivity() {
             Toast.makeText(this@AddActivity, it.toString(), Toast.LENGTH_LONG).show()
         }
 
+        card1.setOnClickListener { dispatchTakePictureIntent() }
+
         customRadioSex.setOnClick { }
     }
 
@@ -33,4 +39,24 @@ class AddActivity : GlobalActivity() {
         menuInflater.inflate(tuppersoft.com.adoptme.R.menu.save, menu)
         return true
     }
+
+    private lateinit var file: Uri
+    private val REQUEST_IMAGE_CAPTURE = 1
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            image1.setImageBitmap(imageBitmap)
+        }
+    }
+
 }
